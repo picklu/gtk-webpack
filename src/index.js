cc7f504bf79ce224726ff7446398150c6ffe55c9
Subrata Sarker <subrata_sarker@yahoo.com>
2019-05-21 22:24:33 +0900

    Log __dirname, __filename, path.join and path.resolve

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import path from 'path';
 import { add, subtract } from './models/model';
 import { display } from './views/view';
+
+console.log(__dirname);
+console.log(__filename);
+console.log(path.resolve(__dirname, __filename));
+console.log(path.join(__dirname, __filename));
 
 display(add, 10, 20);
 display(subtract, 20, 10);
